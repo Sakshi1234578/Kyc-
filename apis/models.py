a551d85f75ffefe410e8b3f58cd468bc69589c9d
Bhuvan_Parashar <mishrasakshi076@gmail.com>
2023-04-04 13:25:37 +0530

    error

--- apis/models.py
+++ apis/models.py
@@ -6,5 +6,5 @@ from apis.database_models.lookup_role import lookup_role
 from apis.database_models.client_super_master import client_super_master
 from apis.database_models.bank_master import bank_master
 from apis.database_models.lookup_state import lookup_state
-from apis.database_models.client_account_details import client_account_details
+#from apis.database_models.client_account_details import client_account_details
 
